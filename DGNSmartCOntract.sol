17026915c38fddfb006a2ddfcfc0ea2135512e12
syedwaheedulla <104793275+syedwaheedulla@users.noreply.github.com>
2023-08-09 00:20:07 +0530

    Update DGNSmartCOntract.sol

--- DGNSmartCOntract.sol
+++ DGNSmartCOntract.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol"; // Import the console module
 
 contract DegenToken is ERC20, Ownable {
+    event PrizeRedeemed(address indexed user, uint256 prizeSelection, uint256 prizeCost);
+
     constructor() ERC20("Degen", "DGN") {}
 
     function mint(address to, uint256 amount) external onlyOwner {
@@ -15,10 +18,23 @@ contract DegenToken is ERC20, Ownable {
         _burn(msg.sender, amount);
     }
 
-    function redeemTokens(uint256 amount) external {
+    function calculatePrizeCost(uint256 prizeSelection) internal pure returns (uint256) {
+        // Implement your prize cost logic here based on prizeSelection
+        // Replace this with your own logic
+        return prizeSelection * 100;
+    }
+
+    function redeemTokens(uint256 amount, uint256 prizeSelection) external {
         require(amount > 0, "Amount must be greater than zero");
         require(balanceOf(msg.sender) >= amount, "Insufficient balance");
 
-        _burn(msg.sender, amount);
+        uint256 prizeCost = calculatePrizeCost(prizeSelection);
+        require(amount >= prizeCost, "Insufficient tokens for selected prize");
+
+        _burn(msg.sender, prizeCost);
+
+
+
+        console.log("Redemption successful for user %s. Prize selection: %s, Prize cost: %s", msg.sender, prizeSelection, prizeCost);
     }
 }
